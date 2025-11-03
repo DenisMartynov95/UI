b84a471c551099635bfc3714bbed438157c31169
Denis Martynov <idenismartynov@yandex.ru>
2025-11-03 17:17:17 +0300

    Настроил все, теперь настраиваю фреймворк

---
+++ src/test/java/PageObjects/LoginPages/LoginPage.java
@@ -0,0 +1,4 @@
+package PageObjects.LoginPages;
+
+public class LoginPage {
+}
---
+++ src/test/java/PageObjects/MI/MainPageSI.java
@@ -0,0 +1,29 @@
+package PageObjects.MI;
+
+import org.openqa.selenium.WebDriver;
+
+public class MainPageSI {
+
+    private final WebDriver driver;
+    public MainPageSI(WebDriver driver) { this.driver = driver; }
+
+    public final String si_mainPage = "https://software.informer.com/";
+
+
+        /*
+            Блок локаторов
+                                     */
+
+
+
+
+
+
+
+
+        /*
+            Блок методов
+                                     */
+
+
+}
