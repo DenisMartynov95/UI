20a134add93d2e03676b18c010f8fadef87d1b5d
Denis Martynov <idenismartynov@yandex.ru>
2026-03-23 17:06:37 +0300

    Есть нужда во внедрении интерфейса

---
+++ src/test/java/PageObjects/MI/MainPageMI_interface.java
@@ -0,0 +1,9 @@
+package PageObjects.MI;
+
+import PageObjects.SI.MainPageSI;
+
+public interface MainPageMI_interface {
+
+    // Интерфейс для проверки "Открыта ли страница MI?
+    MainPageSI checkMiPageIsOpen();
+}
